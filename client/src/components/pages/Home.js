6f025a49e567638bfd5a6a9a8926e5fc4fb373cd
Annie Wang <annie@dhcp-10-29-122-119.dyn.mit.edu>
2022-01-12 18:44:01 -0500

    added home page and css

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "@reach/router";
+
+import "../../utilities.css";
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div>
+            <Link to="/" className="RegisterButton">
+                start planting!
+            </Link>
+        </div>
+    )
+};
+
+export default Home;
