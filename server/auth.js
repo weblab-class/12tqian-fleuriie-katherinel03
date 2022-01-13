6e697b104909b7da8053943a836759bc8217181d
Katherine Liu <shigongliu@dhcp-10-31-123-39.dyn.mit.edu>
2022-01-13 14:29:35 -0500

    auth

--- server/auth.js
+++ server/auth.js
@@ -4,7 +4,7 @@ const socketManager = require("./server-socket");
 
 // create a new OAuth client used to verify google sign-in
 //    TODO: replace with your own CLIENT_ID
-const CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const CLIENT_ID = "714676168299-boi39i597g1mjus8btj5khnp6q8tic4k.apps.googleusercontent.com";
 const client = new OAuth2Client(CLIENT_ID);
 
 // accepts a login token from the frontend, and verifies that it's legit
