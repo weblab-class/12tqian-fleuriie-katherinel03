5ba1ed0a1ea66da94f7c41694c0d732827610427
Annie Wang <annie@dhcp-10-29-122-119.dyn.mit.edu>
2022-01-19 11:47:42 -0500

    should fix login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -33,7 +33,7 @@ const App = () => {
     console.log(`Logged in as ${res.profileObj.name}`);
     const userToken = res.tokenObj.id_token;
     post("/api/login", { token: userToken }).then((user) => {
-      this.setUserId(user._id);
+      setUserId(user._id);
       post("/api/initsocket", { socketid: socket.id });
       console.log(user);
     });
