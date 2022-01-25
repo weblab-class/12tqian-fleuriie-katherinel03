e9c0f697bc7c398f0fd5a3959f75a256095d2103
Katherine Liu <shigongliu@dhcp-10-31-123-39.dyn.mit.edu>
2022-01-25 14:01:29 -0500

    button

--- client/src/components/pages/Garden/RepresentationPopup.js
+++ client/src/components/pages/Garden/RepresentationPopup.js
@@ -5,6 +5,7 @@ import Collapsible from 'react-collapsible';
 import Popup from 'reactjs-popup';
 import PairInteractionPopup from "./PairInteractionPopup";
 import "./RepresentationPopup.css";
+import HelpButton from '/client/src/components/modules/HelpButton.js';
 
 
 const RepresentationPopup = (props) => {
@@ -19,7 +20,11 @@ const RepresentationPopup = (props) => {
 					<button className="close" onClick={close}>
 						&times;
 					</button>
-					<div className="pair-interaction-header"> Pair Interaction </div>
+					<div className="pair-interaction-header"> Pair Interaction 				<HelpButton 
+					helpHeader={'Logging activities'}
+					helpDescription={'Congrats on making a friend! As you log activities with them, the plant symbolizing your relationship will grow and level up, earning you money to buy customization options with. At levels 5 and 10, you will unlock a special new appearance for your plant! You can also switch to different plants by clicking the change plant button. If you want to delete this friend, scroll down to the bottom of the popup and click the Delete Friend button!'}
+					/>
+ </div>
 					<div className="content">
 						<PairInteractionPopup userGoogleID={props.userGoogleID} otherGoogleID={props.otherGoogleID} />
 					</div>
