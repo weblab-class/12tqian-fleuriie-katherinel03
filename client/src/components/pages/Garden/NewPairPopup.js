12eb85d49b5d4333a9619b1633e5a57b22996f23
12tqian <12tqian@gmail.com>
2022-01-26 11:22:27 -0500

    huh

--- client/src/components/pages/Garden/NewPairPopup.js
+++ client/src/components/pages/Garden/NewPairPopup.js
@@ -23,7 +23,7 @@ const NewPairPopup = (props) => {
 						<div className="help-button-div">
 							<HelpButton
 								helpHeader={'Adding new friends'}
-								helpDescription={'To add a new friend, input the name of your friend, their friend code if they have an account with us already (friend code can be found at the bottom of your profile page), and the number of days you want between interactions with them. Once you have made your friends, you can log new activities by clicking on their names!'}
+								helpDescription={'To add a new friend, input the name of your friend, their friend code if they have an account with us already (friend code can be found at the bottom of their profile page), and the number of days you want between interactions with them. Once you have made your friends, you can log new activities by clicking on their names!'}
 							/>
 						</div>
 						</div>
